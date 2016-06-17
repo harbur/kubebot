48c4b0730a1c8c8ba197c488097a45edd1d9e20f
Gil Casadevall <gil@ixde.net>
2016-06-17 14:22:56 +0200

    Added implementation of commands whitelist

--- kubebot.go
+++ kubebot.go
@@ -11,13 +11,16 @@ type Kubebot struct {
 	token    string
 	admins   map[string]bool
 	channels map[string]bool
+	commands map[string]bool
 }
 
 const (
 	forbiddenUserMessage     string = "%s - ⚠ kubectl forbidden for user @%s\n"
 	forbiddenChannelMessage  string = "%s - ⚠ Channel %s forbidden for user @%s\n"
+	forbiddenCommandMessage  string = "%s - ⚠ Command %s forbidden for user @%s\n"
 	forbiddenUserResponse    string = "Sorry @%s, but you don't have permission to run this command :confused:"
 	forbiddenChannelResponse string = "Sorry @%s, but I'm not allowed to run this command here :zipper_mouth_face:"
+	forbiddenCommandResponse string = "Sorry @%s, but I cannot run this command. I'm allowed to run `%s`"
 	okResponse               string = "Roger that!\n@%s, this is the response to your request:\n ```\n%s\n``` "
 )
 
@@ -34,6 +37,11 @@ func kubectl(command *bot.Cmd) (msg string, err error) {
 	if !kb.channels[command.Channel] {
 		fmt.Printf(forbiddenChannelMessage, time, command.Channel, nickname)
 		return fmt.Sprintf(forbiddenChannelResponse, nickname), nil
+	}
+
+	if len(command.Args) > 0 && !kb.commands[command.Args[0]] {
+		fmt.Printf(forbiddenCommandMessage, time, kb.commands, nickname)
+		return fmt.Sprintf(forbiddenCommandResponse, nickname, kb.commands), nil
 	}
 
 	output := execute("kubectl", command.Args...)
--- main.go
+++ main.go
@@ -9,6 +9,7 @@ const (
 	slackTokenLabel    string = "KUBEBOT_SLACK_TOKEN"
 	slackChannelsLabel string = "KUBEBOT_SLACK_CHANNELS_IDS"
 	slackAdminsLabel   string = "KUBEBOT_SLACK_ADMINS_NICKNAMES"
+	slackCommandsLabel string = "KUBEBOT_SLACK_VALID_COMMANDS"
 )
 
 var (
@@ -20,6 +21,7 @@ func main() {
 		token:    os.Getenv(slackTokenLabel),
 		admins:   stringToMap(os.Getenv(slackAdminsLabel), " "),
 		channels: stringToMap(os.Getenv(slackChannelsLabel), " "),
+		commands: stringToMap(os.Getenv(slackCommandsLabel), " "),
 	}
 
 	slack.Run(kb.token)
